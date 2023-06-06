a0f276773a88040ad68b015d5cc96041f0387bef
91994 <mahendra.cp1988@gmail.com>
2023-06-06 10:49:05 +0530

    Updating https params

--- src/main/java/com/piersoft/vm/VmApplication.java
+++ src/main/java/com/piersoft/vm/VmApplication.java
@@ -1,7 +1,12 @@
 package com.piersoft.vm;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,8 +15,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc
 public class VmApplication {
 
+	@Value("${server.http.port:9060}")
+	private int port;
+
 	public static void main(String[] args) {
 		SpringApplication.run(VmApplication.class, args);
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(port);
+		return connector;
 	}
 
 }
--- src/main/java/com/piersoft/vm/request/dto/OnboardVendorDTO.java
+++ src/main/java/com/piersoft/vm/request/dto/OnboardVendorDTO.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -14,11 +15,16 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class OnboardVendorDTO {
 
-    @NotNull(message = "GST Number cannot be null")
+    @NotEmpty(message = "GST Number cannot be null or empty")
     private String gst;
+    @NotEmpty(message = "Aadhaar Number cannot be null or empty")
     private String aadhaar;
+    @NotEmpty(message = "PAN Number cannot be null or empty")
     private String pan;
+    @NotEmpty(message = "Point of contact name cannot be null or empty")
     private String pocName;
+    @NotEmpty(message = "Point of whatsapp Number cannot be null or empty")
     private String pocWhatsappNo;
+    @NotEmpty(message = "Point of contact email cannot be null or empty")
     private String pocEmail;
 }
